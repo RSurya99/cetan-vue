8e8f5f360806bb8885ce41104dc34eb0f2ac6add
RSurya99 <rafli.rfsp@gmail.com>
2022-02-16 11:16:33 +0700

    feat: add lib pusher-js laravel-echo and setup environment variables

--- src/main.ts
+++ src/main.ts
@@ -7,5 +7,14 @@ import store from './store'
 import './assets/css/app.scss'
 import './assets/css/vendor.css'
 import 'nprogress/nprogress.css'
+import Echo from 'laravel-echo'
+import Pusher from 'pusher-js'
+
+window.Echo = new Echo({
+  broadcaster: 'pusher',
+  key: import.meta.env.VITE_PUSHER_KEY,
+  cluster: import.meta.env.VITE_PUSHER_CLUSTER,
+  forceTLS: true,
+})
 
 createApp(App).use(router).use(store).mount('#app')
