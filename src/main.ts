87dc6e1c38f6eab3930791ecc4c7ea08bfc83fea
RSurya99 <rafli.rfsp@gmail.com>
2022-02-16 10:32:30 +0700

    feat: add lib headlessui vueuse axios nprogress and setup axios services

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// css
 import './assets/css/vendor.css'
+import 'nprogress/nprogress.css'
 
 createApp(App).use(router).mount('#app')
