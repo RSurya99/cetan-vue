1d694d24de42c19fbb38258e53e2df7996f2285c
RSurya99 <rafli.rfsp@gmail.com>
2022-03-01 16:11:54 +0700

    feat: add middleware isAuthenticated and hideForAuth using meta

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import NProgress from 'nprogress'
 import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '@/stores/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,18 +9,55 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: () => import('../views/HomeView.vue'),
+      meta: {
+        authenticated: true,
+      },
     },
     {
       path: '/login',
       name: 'login',
       component: () => import('../views/Auth/SignIn.vue'),
+      meta: {
+        hideForAuth: true,
+      },
     },
     {
       path: '/register',
       name: 'register',
       component: () => import('../views/Auth/SignUp.vue'),
+      meta: {
+        hideForAuth: true,
+      },
     },
   ],
+})
+
+// isAuthenticated meta
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+  if (to.matched.some((record) => record.meta.authenticated)) {
+    if (authStore.isAuthenticated) {
+      next()
+    } else {
+      next({ path: '/login' })
+    }
+  } else {
+    next()
+  }
+})
+
+// hideForAuth meta
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+  if (to.matched.some((record) => record.meta.hideForAuth)) {
+    if (authStore.isAuthenticated) {
+      next({ path: '/' })
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
 })
 
 router.beforeEach(() => {
