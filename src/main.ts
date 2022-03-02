e057f6f603b47f2bf69013441307fdc8251124d8
RSurya99 <rafli.rfsp@gmail.com>
2022-03-03 01:45:19 +0700

    feat: add lib laravel-echo & pusher-js also configure it and add environment variables

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,16 @@ import router from './router'
 import './assets/sass/app.scss'
 import './assets/css/vendor.css'
 
+import Echo from 'laravel-echo'
+import Pusher from 'pusher-js'
+
+window.Echo = new Echo({
+  broadcaster: 'pusher',
+  key: import.meta.env.VITE_PUSHER_KEY,
+  cluster: import.meta.env.VITE_PUSHER_CLUSTER,
+  forceTLS: true,
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
