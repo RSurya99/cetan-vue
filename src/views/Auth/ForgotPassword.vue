3f1c5db3963016af6b65e5f6a96ebef139a041b0
RSurya99 <rafli.rfsp@gmail.com>
2022-03-03 14:28:02 +0700

    refactor: update text message in ForgotPassword

--- src/views/Auth/ForgotPassword.vue
+++ src/views/Auth/ForgotPassword.vue
@@ -2,7 +2,7 @@
   <PageAuthBaseWrapper>
     <PageAuthBaseSection>
       <h1 class="text-3xl font-bold text-emerald-500 leading-tight text-center">
-        Do you really think I will create a forgot password feature?
+        Do you really think we will create a forgot password feature?
       </h1>
       <BuilderBaseButton text="Go Back to Sign In Page" to="/login" />
     </PageAuthBaseSection>
